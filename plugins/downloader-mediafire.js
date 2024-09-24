d88b83ffaaddca47b8316ab1b8569699f866c087
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-24 13:48:10 +0200

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,17 +1,17 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingrese el enlace de un archivo de Mediafire.', m, rcanal)
-if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, 'El enlace deve ser de un archivo de Mediafire.', m, rcanal)
+if (!args[0]) return conn.reply(m.chat, '🐯 Enter the link of a Mediafire file.', m, rcanal)
+if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, 'The link must be to a Mediafire file.', m, rcanal)
 await m.react('🕓')
 try {
 let { title, ext, aploud, size, dl_url } = await Starlights.mediafire(args[0])
 let txt = `乂  *M E D I A F I R E  -  D O W N L O A D*\n\n`
-    txt += `	✩  *Nombre* : ${title}\n`
-    txt += `	✩  *Peso* : ${size}\n`
-    txt += `	✩  *Publicado* : ${aploud}\n`
+    txt += `	✩  *Name* : ${title}\n`
+    txt += `	✩  *Weight* : ${size}\n`
+    txt += `	✩  *Published* : ${aploud}\n`
     txt += `	✩  *MimeType* : ${ext}\n\n`
-    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lenta. . .*`
+    txt += `*- ↻ The file is being sent, please wait a moment, I'm slow. . .*`
 let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
@@ -20,11 +20,11 @@ await m.react('✅')
 try {
 let { title, ext, aploud, size, dl_url } = await Starlights.mediafireV2(args[0])
 let txt = `乂  *M E D I A F I R E  -  D O W N L O A D*\n\n`
-    txt += `	✩  *Nombre* : ${title}\n`
-    txt += `	✩  *Peso* : ${size}\n`
-    txt += `	✩  *Publicado* : ${aploud}\n`
+    txt += `	✩  *Name* : ${title}\n`
+    txt += `	✩  *Weight* : ${size}\n`
+    txt += `	✩  *Published* : ${aploud}\n`
     txt += `	✩  *MimeType* : ${ext}\n\n`
-    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lenta. . .*`
+    txt += `*- ↻ The file is being sent, please wait a moment, I'm slow. . .*`
 let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
