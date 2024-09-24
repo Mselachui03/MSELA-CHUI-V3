13d4961354bad3d968909dee234c884e4d4ee2f8
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-24 13:34:11 +0200

    Update downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, { args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `🚩 Escribe la URL de un repositorio de GitHub que deseas descargar.`, m)
+    return conn.reply(m.chat, `🐯 Enter the URL of a GitHub repository you want to download.`, m)
   }
   if (!regex.test(args[0])) {
     return conn.reply(m.chat, `Verifica que la *URL* sea de GitHub`, m).then(_ => m.react('✖️'))
@@ -23,12 +23,12 @@ let handler = async (m, { args, usedPrefix, command }) => {
     let type = zipResponse.headers.get('content-type')
     let img = 'https://i.ibb.co/tLKyhgM/file.png'
     let txt = `*乂  G I T H U B  -  D O W N L O A D*\n\n`
-       txt += `	✩  *Nombre* : ${filename}\n`
-       txt += `	✩  *Repositorio* : ${user}/${sanitizedRepo}\n`
-       txt += `	✩  *Creador* : ${repoData.owner.login}\n`
-       txt += `	✩  *Descripción* : ${repoData.description || 'Sin descripción disponible'}\n`
+       txt += `	✩  *Name* : ${filename}\n`
+       txt += `	✩  *Repository* : ${user}/${sanitizedRepo}\n`
+       txt += `	✩  *Creator* : ${repoData.owner.login}\n`
+       txt += `	✩  *Description* : ${repoData.description || 'No description available'}\n`
        txt += `	✩  *Url* : ${args[0]}\n\n`
-       txt += `🚩 *${textbot}*`
+       txt += `🐯 *${textbot}*`
 
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await conn.sendFile(m.chat, await zipResponse.buffer(), filename, null, m)
