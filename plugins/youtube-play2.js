62abfd173ec20e964a197c6c10519e645c1eefc1
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-24 22:43:06 +0200

    Update youtube-play2.js

--- plugins/youtube-play2.js
+++ plugins/youtube-play2.js
@@ -7,22 +7,22 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
 let lister = ["mp3", "yta", "audio", "ytv", "video", "vídeo", "mp4", "mp3doc", "ytadoc", "audiodoc", "mp4doc", "ytvdoc", "videodoc", "vídeodoc"]
 
 let [feature, inputs, inputs_, inputs__, inputs___] = text.split(" ")
-if (!lister.includes(feature)) return conn.reply(m.chat, `🚩 Ingresa el formato en que deseas descargar más el titulo de un video o musica de YouTube.\n\nEjemplo : ${usedPrefix + command} *mp3* SUICIDAL-IDOL - ecstacy\n\nFormatos disponibles :\n${usedPrefix + command} *mp3*\n${usedPrefix + command} *mp3doc*\n${usedPrefix + command} *mp4*\n${usedPrefix + command} *mp4doc*`, m, rcanal)
+if (!lister.includes(feature)) return conn.reply(m.chat, `🐯 Enter the format in which you want to download plus the title of a YouTube video or music.\n\nExample : ${usedPrefix + command} *mp3* SUICIDAL-IDOL - ecstacy\n\nAvailable formats :\n${usedPrefix + command} *mp3*\n${usedPrefix + command} *mp3doc*\n${usedPrefix + command} *mp4*\n${usedPrefix + command} *mp4doc*`, m, rcanal)
 if (lister.includes(feature)) {
 if (feature == "mp3" || feature == "yta" || feature == "audio") {
-if (!inputs) return conn.reply(m.chat, '🚩 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m, rcanal)
+if (!inputs) return conn.reply(m.chat, '🐯 Enter the title of a YouTube video or song.\n\n`Example:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m, rcanal)
 await m.react('🕓')
 let res = await yts(text)
 let vid = res.videos[0]
 let q = '128kbps'
 let txt = '`乂  Y O U T U B E  -  P L A Y`\n\n'
-	txt += `	✩   *Título* : ${vid.title}\n`
-	txt += `	✩   *Duración* : ${vid.timestamp}\n`
-	txt += `	✩   *Visitas* : ${toNum(vid.views)} ( *${formatNumber(vid.views)}* )\n`
-	txt += `	✩   *Autor* : ${vid.author.name}\n`
-	txt += `	✩   *Publicado* : ${eYear(vid.ago)}\n`
+	txt += `	✩   *Title* : ${vid.title}\n`
+	txt += `	✩   *Duration* : ${vid.timestamp}\n`
+	txt += `	✩   *Visits* : ${toNum(vid.views)} ( *${formatNumber(vid.views)}* )\n`
+	txt += `	✩   *Author* : ${vid.author.name}\n`
+	txt += `	✩   *Published* : ${eYear(vid.ago)}\n`
 	txt += `	✩   *Url* : ${'https://youtu.be/' + vid.videoId}\n\n`
-	txt += `> *- ↻ El audio se esta enviando espera un momento, soy lenta. . .*`
+	txt += `> *- ↻ Audio is being sent, please wait a moment, I'm slow. . .*`
 await conn.sendFile(m.chat, vid.thumbnail, 'thumbnail.jpg', txt, m, null, rcanal)
 try {
 let yt = await fg.yta(vid.url, q)
