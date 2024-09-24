7e9ef9b2d1fb4631f3a2d3a55bff911db46b3405
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-24 15:38:01 +0200

    Update downloader-tiktokvid.js

--- plugins/downloader-tiktokvid.js
+++ plugins/downloader-tiktokvid.js
@@ -1,22 +1,22 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, '🚩 Ingresa un texto junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Ai Hoshino Edit`, m, rcanal)
+  if (!text) return conn.reply(m.chat, '🐯 Enter some text next to the command.\n\n`Example:`\n' + `> *${usedPrefix + command}* MSELA-CHUI-V3 Edit`, m, rcanal)
   await m.react('🕓')
   try {
     let { title, author, duration, views, likes, comments_count, share_count, download_count, published, dl_url } = await Starlights.tiktokvid(text)
 
       let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
-          txt += `    ✩  *Título* : ${title}\n`
-          txt += `    ✩  *Autor* : ${author}\n`
-          txt += `    ✩  *Duración* : ${duration} segundos\n`
-          txt += `    ✩  *Vistas* : ${views}\n`
+          txt += `    ✩  *Title* : ${title}\n`
+          txt += `    ✩  *Author* : ${author}\n`
+          txt += `    ✩  *Duration* : ${duration} segundos\n`
+          txt += `    ✩  *Views* : ${views}\n`
           txt += `    ✩  *Likes* : ${likes}\n`
-          txt += `    ✩  *Comentarios* : ${comments_count}\n`
-          txt += `    ✩  *Compartidos* : ${share_count}\n`
-          txt += `    ✩  *Publicado* : ${published}\n`
-          txt += `    ✩  *Descargas* : ${download_count}\n\n`
-          txt += `> 🚩 ${textbot}`
+          txt += `    ✩  *Comments* : ${comments_count}\n`
+          txt += `    ✩  *shared* : ${share_count}\n`
+          txt += `    ✩  *Published* : ${published}\n`
+          txt += `    ✩  *Downloads* : ${download_count}\n\n`
+          txt += `> 🐯 ${textbot}`
 
       await conn.sendFile(m.chat, dl_url, `thumbnail.mp4`, txt, m)
       await m.react('✅')
@@ -25,7 +25,7 @@ let handler = async (m, { conn, usedPrefix, command, text, args }) => {
     await m.react('✖️')
   }
 }
-handler.help = ['tiktokvid *<búsqueda>*']
+handler.help = ['tiktokvid *<search>*']
 handler.tags = ['downloader']
 handler.command = ['ttvid', 'tiktokvid']
 handler.register = true
