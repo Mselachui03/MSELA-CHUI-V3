4a0b02b515d23e5e41cdef8d43cf7d791bbb4aea
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-24 13:04:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -31,10 +31,10 @@ global.prems = []
 
 global.packname = `MSELA-CHUI-V3`
 global.author = '{\n "bot": {\n   "name": "MSELA-CHUI-V3",\n     "author": "Mselachui",\n   "status_bot": "active"\n }\n}'
-global.wait = '🐯 *Please wait brother... ฅ^•ﻌ•^ฅ*'
+global.wait = '🐯 *Please wait brother... MSELA-CHUI-V3*'
 global.botname = '✯ MSELA-CHUI-V3 ✰'
 global.textbot = `Powered by MSELA-CHUI-V3 and lazack_28`
-global.listo = '*Aqui tiene ฅ^•ﻌ•^ฅ*'
+global.listo = '*MSELA-CHUI-V3*'
 global.namechannel = '【 ✯MSELA-CHUI-V3 CHANNEL✰ 】'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
