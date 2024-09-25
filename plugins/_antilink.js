e529917a46e578f2407d658796cf04b4d6c70e51
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-25 13:34:40 +0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,3 +1,4 @@
+
 const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 
 export async function before(m, {conn, isAdmin, isBotAdmin }) {
@@ -13,11 +14,14 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `🐯 We don't allow links from other groups, sorry *@${m.sender.split('@')[0]}* you will be kicked out from the group ${isBotAdmin ? '' : '\n\nI'm not an admin so I can't expel you :"v'}`, null, { mentions: [m.sender] } )
+        await conn.reply(m.chat, `*≡ Enlace Detectado*
+            
+No permitimos enlaces de otros grupos 
+lo siento *@${m.sender.split('@')[0]}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
         } else if (!chat.antiLink) return //m.reply('')
     }
     return !0
-        }
+}
