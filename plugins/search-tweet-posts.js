bc1c203b63f5fa27ce1402ff3759eda56c722d79
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-25 07:29:44 +0200

    Update search-tweet-posts.js

--- plugins/search-tweet-posts.js
+++ plugins/search-tweet-posts.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
 
 let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 Ingrese el texto del post que deseas buscar.', m, rcanal)
+if (!text) return conn.reply(m.chat, '🐯 Enter the text of the post you want to search for.', m, rcanal)
 await m.react('🕓')
 try {
 let json = await axios.get(`https://apis-starlights-team.koyeb.app/starlight/Twitter-Posts?text=${text}`, { headers: { 'Content-Type': 'application/json' }})
 let result = json.data.result
-if (!result || !result.length) return conn.reply(m.chat, `No se encontraron resultados.`, m, rcanal)
+if (!result || !result.length) return conn.reply(m.chat, `No results found.`, m, rcanal)
         
 let txt = `*乂  T W I T T E R  -  S E A R C H*`
     result.forEach(({ user, post, profile, user_link }, index) => {
     txt += `\n\n`
-    txt += `  *» Nro* : ${index + 1}\n`
+    txt += `  *» Dream* : ${index + 1}\n`
     txt += `  *» User* : ${user}\n`
-    txt += `  *» Publicacion* : ${post}\n`
-    txt += `  *» Perfil* : ${profile}\n`
+    txt += `  *» Publication* : ${post}\n`
+    txt += `  *» Profile* : ${profile}\n`
     txt += `  *» Link* : ${user_link}`
     })
 await conn.reply(m.chat, txt, m, rcanal)
@@ -22,7 +22,7 @@ await m.react('✅')
 } catch {
 await m.react('✖️')
 }}
-handler.help = ['tweetposts *<búsqueda>*']
+handler.help = ['tweetposts *<search>*']
 handler.tags = ['search']
 handler.command = ['tweetposts']
 handler.register = true 
