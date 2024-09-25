9a9ac22831830963019775b0f8e8af6c2399eb69
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-25 07:11:44 +0200

    Update search-spotify.js

--- plugins/search-spotify.js
+++ plugins/search-spotify.js
@@ -1,7 +1,7 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-  if (!text) return conn.reply(m.chat, '🚩 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m, rcanal)
+  if (!text) return conn.reply(m.chat, '🐯 Enter the title of a YouTube video or song.\n\n`Example:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m, rcanal)
   await m.react('🕓')
   try {
     let res = await Starlights.spotifySearch(text)
@@ -9,9 +9,9 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
     let txt = '`乂  S P O T I F Y  -  S E A R C H`'
     for (let i = 0; i < res.length; i++) {
       txt += `\n\n`
-      txt += `  *» Nro* : ${i + 1}\n`
-      txt += `  *» Titulo* : ${res[i].title}\n`
-      txt += `  *» Artista* : ${res[i].artist}\n`
+      txt += `  *» Dream* : ${i + 1}\n`
+      txt += `  *» title* : ${res[i].title}\n`
+      txt += `  *» Artist* : ${res[i].artist}\n`
       txt += `  *» Url* : ${res[i].url}`
     }
     
@@ -20,7 +20,7 @@ await m.react('✅')
 } catch {
 await m.react('✖️')
 }}
-handler.help = ['spotifysearch *<búsqueda>*']
+handler.help = ['spotifysearch *<search>*']
 handler.tags = ['search']
 handler.command = ['spotifysearch']
 handler.register = true
