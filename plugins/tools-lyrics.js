1381ffe53e262884dbf65333b9254655ae6ba8df
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-26 23:10:24 +0200

    Update tools-lyrics.js

--- plugins/tools-lyrics.js
+++ plugins/tools-lyrics.js
@@ -1,30 +1,22 @@
-import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
+import fg from 'api-dylux'
+let handler = async (m, {conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-  if (!teks) throw `✳️ Enter the name of the song`
-  try {
-    let res = await fetch(global.API('https://some-random-api.com', '/lyrics', { title: teks }))
-    if (!res.ok) throw await res.text()
-    let json = await res.json()
-    if (!json.thumbnail.genius) throw json
-    conn.sendFile(
-      m.chat,
-      json.thumbnail.genius,
-      null,
-      `
-▢ *${json.title}*
-*${json.author}*\n
-${json.lyrics}`,
-      m
-    )
-    m.react(done)
-  } catch (e) {
-    m.react(error)
-  }
+   if (!teks) throw `✳️ ${mssg.notext}`
+   try {
+ let res = await fg.lyrics(text);
+ let mes = `▢ *${res.title}*
+▢ *${res.artist}*
+
+▢ ${res.lyrics}`;
+    conn.sendFile(m.chat, res.image, 'img.png', mes, m);
+} catch (e) {
+	m.react(error)
+} 
+
 }
 handler.help = ['lyrics']
 handler.tags = ['tools']
-handler.command = ['letra', 'lyrics', 'letras']
+handler.command = ['letter', 'lyrics', 'letters'] 
 
 export default handler
