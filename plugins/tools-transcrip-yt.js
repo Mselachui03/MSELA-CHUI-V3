5fbff9065b80fbeadb3c281d4b7e5d624728e6b8
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-26 11:23:40 +0200

    Update tools-transcrip-yt.js

--- plugins/tools-transcrip-yt.js
+++ plugins/tools-transcrip-yt.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 *Ingrese la url de la cancion de YouTube*', m, rcanal)
+if (!text) return conn.reply(m.chat, '🐯 *Enter the url of the YouTube song*', m, rcanal)
 await m.react('🕓')
 try {
 let app = await fetch(`https://apis-starlights-team.koyeb.app/starlight/transcribir-youtube?url=${text}`, { headers: { 'Content-Type': 'application/json' }})
