b4a9710fabcedce0d75cebb2f7e812cfaffdc79e
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-26 10:44:10 +0200

    Update serbot-code.js

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -85,16 +85,16 @@ if (methodCode && !conn.authState.creds.registered) {
     setTimeout(async () => {
         let codeBot = await conn.requestPairingCode(cleanedNumber);
         codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot;
-        let txt = ` –  *S E R B O T  -  S U B B O T*\n\n`
-            txt += `┌  ✩  *Usa este Código para convertirte en un Sub Bot*\n`
-            txt += `│  ✩  Pasos\n`
-            txt += `│  ✩  *1* : Haga click en los 3 puntos\n`
-            txt += `│  ✩  *2* : Toque dispositivos vinculados\n`
-            txt += `│  ✩  *3* : Selecciona *Vincular con el número de teléfono*\n` 
-            txt += `└  ✩  *4* : Escriba el Codigo\n\n`
-            txt += `*Nota:* Este Código solo funciona en el número que lo solicito`
-         await parent.reply(m.chat, txt, m, rcanal)
-         await parent.reply(m.chat, codeBot, m, rcanal)
+        let txt = ` – *S E R B O T - S U B B O T*\n\n`
+            txt += `┌ ✩ *Use this Code to become a Sub Bot*\n`
+            txt += `│ ✩ Steps\n`
+            txt += `│ ✩ *1* : Click on the 3 dots\n`
+            txt += `│ ✩ *2* : Touch paired devices\n`
+            txt += `│ ✩ *3* : Select *Link to phone number*\n` 
+            txt += `└ ✩ *4* : Enter the Code\n\n`
+            txt += `*Note:* This Code only works on the number I requested it`
+         await parent.reply(m.chat, txt, m, channel)
+         await parent.reply(m.chat, codeBot, m, channel)
         rl.close()
     }, 3000)
 }
