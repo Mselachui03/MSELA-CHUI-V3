6bc8eef900075de671952841f4fdbd50f4f52752
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-27 04:59:52 +0200

    Update tools-translate.js

--- plugins/tools-translate.js
+++ plugins/tools-translate.js
@@ -1,36 +1,38 @@
+
 import { translate } from '@vitalets/google-translate-api'
-const defaultLang = 'en'
+const defaultLang = 'es'
 const tld = 'cn'
 
 let handler = async (m, { args, usedPrefix, command }) => {
-  let err = `
-📌 *Example:*
+    let err = `
+📌 *${mssg.example} :*
 
-*${usedPrefix + command}* <id> [text]
-*${usedPrefix + command}* en Hello World
+*${usedPrefix + command}* <language> [text]
+*${usedPrefix + command}* es Hello World
 
-≡ *List of supported languages:* 
+≡ *${mssg.tradList}:* 
 
 https://cloud.google.com/translate/docs/languages
 `.trim()
 
-  let lang = args[0]
-  let text = args.slice(1).join(' ')
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang
-    text = args.join(' ')
-  }
-  if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 
-  try {
-    let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null)
-    m.reply(result.text)
-  } catch (e) {
-    throw err
-  }
+    try {
+       let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null) 
+       m.reply(result.text)
+    } catch (e) {
+        throw err
+    } 
+
 }
-handler.help = ['translate <lang> <text>']
+handler.help = ['trad <leng> <text>']
 handler.tags = ['tools']
-handler.command = ['translate', 'tl', 'trad', 'tr']
+handler.command = ['translate', 'trt', 'transl', 'tr', 'translate']
 
 export default handler
