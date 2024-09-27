e9819c2854ae5dc9e125d37457e6435194e3af9b
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-27 22:01:47 +0200

    Update group-invite.js

--- plugins/group-invite.js
+++ plugins/group-invite.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `🐯 Enter the number you want to send a group invite to.\n\n*Example:*\n*${usedPrefix + command}* 5218261275256`, m , rcanal)
+if (!text) return conn.reply(m.chat, `🐯 Enter the number you want to send a group invite to.\n\n*Example:*\n*${usedPrefix + command}* 260769355624`, m , rcanal)
 if (text.includes('+')) return conn.reply(`🐯 Enter the number all together without the *+*`, m, rcanal)
 if (isNaN(text)) return conn.reply(m.chat, `*Enter only numbers plus your country code without spaces*`, m, rcachannel)
 let group = m.chat
