98e8ecb823124d0ad770b86a911a0ec752e374ee
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-27 21:13:24 +0200

    Update group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '🚩 Bot activo en este grupo.', m, rcanal)
+    await conn.reply(m.chat, '🐯 Bot active in this group.', m, rcanal)
     await m.react('✅')
 }
 handler.help = ['desbanearbot']
