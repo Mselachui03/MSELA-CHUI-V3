9868d72c34c15af71ff921341faf4a92d5d01cd9
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-27 03:57:28 +0200

    Update tools-react.js

--- plugins/tools-react.js
+++ plugins/tools-react.js
@@ -1,13 +1,13 @@
 let { proto } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-  if (!args[0]) return conn.reply(m.chat, `🚩 Responde a un mensaje y coloca el emoji junto al comando.`, m, rcanal)
+  if (!args[0]) return conn.reply(m.chat, `🐯 Reply to a message and place the emoji next to the command.`, m, rcanal)
   let q = m.quoted ? await m.getQuotedObj() : m
  conn.sendMessage(m.chat, { react: { text: args[0], key: q.key }}, { quoted: q })
 }
 handler.help = ['react *<emoji>*']
 handler.tags = ['tools']
-handler.command = ['react', 'reaccionar', 'reaccion'] 
+handler.command = ['react', 'react', 'reaction'] 
 handler.register = true 
 
 export default handler
