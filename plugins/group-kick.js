4a093ff793133af274ee3bf6a98cd4b7df2149fd
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-27 13:01:09 +0200

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -6,13 +6,13 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply('🐯 Deleted user.`)
+m.reply(`🐯 Deleted user.`)
 m.reply(`Sorry, you've just been removed from the group..`, user)
 }
 
 handler.help = ['kick *@user*']
 handler.tags = ['group']
-handler.command = ['kick', 'remove'] 
+handler.command = ['kick', 'expulsar'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
