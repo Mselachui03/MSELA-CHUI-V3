7b281af1e9a8c3a320c43b748c329c7f54279505
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-09-28 07:27:52 +0200

    Update tools-lyrics.js

--- plugins/tools-lyrics.js
+++ plugins/tools-lyrics.js
@@ -5,10 +5,10 @@ let handler = async (m, {conn, text }) => {
    if (!teks) throw `✳️ ${mssg.notext}`
    try {
  let res = await fg.lyrics(text);
- let mes = `▢ *${res.title}*
-▢ *${res.artist}*
+ let mes = `🐯 *${res.title}*
+🐯 *${res.artist}*
 
-▢ ${res.lyrics}`;
+🐯 ${res.lyrics}`;
     conn.sendFile(m.chat, res.image, 'img.png', mes, m);
 } catch (e) {
 	m.react(error)
