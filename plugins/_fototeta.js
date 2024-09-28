397641e0dbe7b096c174db58bc997e5166822f66
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-29 01:58:31 +0300

    Update _fototeta.js

--- plugins/_fototeta.js
+++ plugins/_fototeta.js
@@ -2,5 +2,5 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 await conn.reply(m.chat,`Our Father, who art in Heaven, hallowed be your name, your Kingdom come, your will be done on earth as it is in heaven. and forgive us our debts as we forgive our debtors, and lead us not into temptation, but deliver us from evil.`, style)
 }
 handler.customPrefix = /^(Fototeta|fototeta)$/i
-handler.command = ['sala']
+handler.command = new RegExp
 export default handler
