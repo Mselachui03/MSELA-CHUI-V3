7fe8e267bf34110297f891238a840c1dc758be6c
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 18:11:00 +0200

    Update rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -2,21 +2,21 @@
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Retirar.')
+   if (!args[0]) return m.reply('🐯 Enter the amount of *⭐ Stars* you wish to Withdraw.')
    if (args[0] == 'all') {
       let count = parseInt(user.bank)
       user.bank -= count * 1
       user.limit += count * 1
-      await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)
+      await m.reply(`🐯 You removed *${count} ⭐ Stars* of the Bank.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('🐯 The amount must be a Number.')
    let count = parseInt(args[0])
-   if (!user.bank) return m.reply('No tienes *⭐ Estrellas* en el Banco.')
-   if (user.bank < count) return m.reply(`Solo tienes *${user.bank} ⭐ Estrellas* en el Banco.`)
+   if (!user.bank) return m.reply('You dont have *⭐ Stars* in the Bank.')
+   if (user.bank < count) return m.reply(`You only have *${user.bank} ⭐ Stars* in the Bank.`)
    user.bank -= count * 1
    user.limit += count * 1
-   await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)
+   await m.reply(`🐯 You removed *${count} ⭐ Stars* of the Bank.`)
 }
 
 handler.help = ['retirar']
