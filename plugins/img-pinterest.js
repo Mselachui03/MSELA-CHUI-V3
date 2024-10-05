aa44e415f1af59feb7aac050201bde07930446fc
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 10:23:09 +0200

    Update img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -1,16 +1,16 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Ai Hoshino Icons`)
+if (!text) return m.reply('🐯 Enter the name of the image you are looking for.\n\n`Example:`\n' + `> *${usedPrefix + command}* MSELA-CHUI-V3 Icons`)
 await m.react('🕓')
 try {
 let { dl_url } = await Starlights.pinterest(text)
-await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
+await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Result* : ${text}`, m, null, rcanal)
 await m.react('✅')
 } catch {
 await m.react('✖️')
 }}
-handler.help = ['pinterest *<búsqueda>*']
+handler.help = ['pinterest *<search>*']
 handler.tags = ['img']
 handler.command = ['pinterest']
 handler.register = true 
