45ff30aad15fd2f51dd0f366c20f804edd9d29c2
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 16:06:36 +0200

    Update owner-otag.js

--- plugins/owner-otag.js
+++ plugins/owner-otag.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants}) => {
 	
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    if (!m.quoted) return m.reply(`🚩 Responde a un mensaje.`)
+    if (!m.quoted) return m.reply(`🐯 Reply to a message.`)
     conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 
 handler.help = ['otag']
 handler.tags = ['group']
-handler.command = /^(otag|tag2)$/i
+handler.command = /^(otag|hidetag)$/i
 
 handler.rowner = true
 handler.group = true
