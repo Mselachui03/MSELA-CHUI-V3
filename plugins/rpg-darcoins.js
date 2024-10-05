c4859e9f91aedffda97a760fe2a6ae59e256e6f1
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 17:39:12 +0200

    Update rpg-darcoins.js

--- plugins/rpg-darcoins.js
+++ plugins/rpg-darcoins.js
@@ -6,26 +6,26 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw '🚩 Menciona al usuario con *@user.*'
+    if (!who) throw '🐯 Mention the user with *@user.*'
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw '🚩 Ingrese la cantidad de *⭐ Estrellas* que quiere transferir.'
-    if (isNaN(txt)) throw 'Sólo números.'
+    if (!txt) throw '🐯 Enter the amount of *⭐ Stars* you want to transfer.'
+    if (isNaN(txt)) throw 'Only numbers.'
     let poin = parseInt(txt)
     let limit = poin
     let imt = Math.ceil(poin * impuesto)
     limit += imt
-    if (limit < 1) throw '🚩 Mínimo es *1 ⭐ Estrella*.'
+    if (limit < 1) throw '🐯 Minimum is *1 ⭐ Star*.'
     let users = global.db.data.users
-    if (limit > users[m.sender].limit) throw 'No tienes suficientes *⭐ Estrellas* para dar.'
+    if (limit > users[m.sender].limit) throw 'You don't have enough *⭐ Stars* to give.'
     users[m.sender].limit -= limit
     users[who].limit += poin
     
-    await m.reply(`*${-poin}* ⭐ Estrellas 
-Impuesto 2% : *${-imt}* ⭐ Estrellas
-Total gastado: *${-limit}* ⭐ Estrellas`)
-    conn.fakeReply(m.chat, `*+${poin}* *⭐ Estrellas.*`, who, m.text)
+    await m.reply(`*${-poin}* ⭐ Stars 
+Impuesto 2% : *${-imt}* ⭐ Stars
+Total gastado: *${-limit}* ⭐ Stars`)
+    conn.fakeReply(m.chat, `*+${poin}* *⭐ Stars.*`, who, m.text)
 }
-handler.help = ['darstars *@user <cantidad>*']
+handler.help = ['darstars *@user <quantity>*']
 handler.tags = ['rpg']
 handler.command = ['darcoins', 'darstars']
 handler.register = true 
