a825db9b21f022f89ebeb50e4cc45c9d759cca66
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 18:06:02 +0200

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -8,12 +8,12 @@ let handler = async (m, { conn }) => {
   let tiempoEspera = 5 * 60
   if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
     let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    conn.reply(m.chat, `🚩 Hola ${name}, Ya has minado recientemente, espera ⏱ *${tiempoRestante}* para regresar a la Mina.`, m, rcanal)
+    conn.reply(m.chat, `🐯 Hello ${name}, You have already mined recently, please wait ⏱ *${tiempoRestante}* to return to the Mine.`, m, rcanal)
     return
   }
   
   global.db.data.users[m.sender].exp += hasil
-  let txt = `🚩 Genial! minaste *${hasil} 💫 XP.*`
+  let txt = `🐯 Brilliant! of me *${hasil} 💫 XP.*`
   await m.react('⛏')
   await conn.reply(m.chat, txt, m, rcanal)
   
@@ -29,5 +29,5 @@ function segundosAHMS(segundos) {
   let horas = Math.floor(segundos / 3600)
   let minutos = Math.floor((segundos % 3600) / 60)
   let segundosRestantes = segundos % 60
-  return `${minutos} minutos y ${segundosRestantes} segundos`
+  return `${minutos} minutos y ${segundosRestantes} seconds`
 }
