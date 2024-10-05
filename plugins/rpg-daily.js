f55538c3d9399a324a30cf1a51ce775aacd17dc4
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 17:05:32 +0200

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -7,12 +7,12 @@ let handler = async (m, { conn, isPrems }) => {
   const tiempoEspera = 24 * 60 * 60
   if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
     const tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    conn.reply(m.chat, `🚩 Ya has realizado tu pedido gratis de hoy.\nRecuerda que solo puedes realizarlo 1 vez cada 24 horas.\n\n*Próximo Monto* : +${isPrems ? prem : free} 💫 XP\n*En* : ⏱ ${tiempoRestante}`, m, rcanal)
-    return
+    conn.reply(m.chat, `🐯 You have already placed your free order for today.\nRemember that you can only place it once every 24 hours.\n\n*Next Amount* : +${isPrems ? prem : free} 💫 XP\n*In* : ⏱ ${timeRemaining}`, m, rcanal) 
+      return 
   }
 
   global.db.data.users[m.sender].exp += isPrems ? prem : free
-  conn.reply(m.chat, `🚩 Felicidades 🎉, reclamaste *+${isPrems ? prem : free} 💫 XP*.`, m, rcanal)
+  conn.reply(m.chat, `🐯 Congratulations 🎉, you claimed *+${isPrems ? prem : free} 💫 XP*.`, m, rcanal)
 
   cooldowns[m.sender] = Date.now()
 }
@@ -28,5 +28,5 @@ function segundosAHMS(segundos) {
   const horas = Math.floor(segundos / 3600)
   const minutos = Math.floor((segundos % 3600) / 60)
   const segundosRestantes = segundos % 60
-  return `${horas} horas, ${minutos} minutos y ${segundosRestantes} segundos`;
+  return `${horas} hours, ${minutos} minutes y ${segundosRestantes} seconds`;
 }
