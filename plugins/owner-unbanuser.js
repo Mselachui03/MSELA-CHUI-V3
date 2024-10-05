0f9962426779daa5bddc84c66f53eba1a4d784fc
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 16:37:07 +0200

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    if (!who) return m.reply(`🐯 Tag a user.`)
     let users = global.db.data.users
     users[who].banned = false
-    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido desbaneado con exito, ahora podrá volver a usar mis comandos.`, m, { mentions: [who] })
-}
-handler.help = ['munban *@user*']
+    conn.reply(m.chat, `🐯 @${who.split`@`[0]} You have been successfully unbanned, you can now use my commands again.`, m, { mentions: [who] })
+       }
+handler.help = ['unban *@user*']
 handler.tags = ['owner']
-handler.command = /^munban$/i
+handler.command = /^unban$/i
 handler.rowner = true
 
 export default handler
