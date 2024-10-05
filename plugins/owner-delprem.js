e408b65aa8a7f329a803827ff8896683a320ac70
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 13:09:43 +0200

    Update owner-delprem.js

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -3,10 +3,10 @@ let handler = async (m, { usedPrefix, command, text }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
     else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
     let user = db.data.users[who]
-    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    if (!who) return m.reply(`🐯 Tag a user.`)
     user.premium = false
     user.premiumTime = 0
-    m.reply(`🚩 El usuario *${user.name}* ya no sera *Premium.*`)
+    m.reply(`🐯 The user *${user.name}* It will no longer be *Premium.*`)
 }
 handler.help = ['delprem *@user*']
 handler.tags = ['owner']
