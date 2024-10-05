6bd700aa972585b352e4db9e92ed0b707dded224
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 11:51:26 +0200

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -3,17 +3,17 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = db.data.users[who]
-    if (!who) return m.reply(`Etiqueta o menciona a alguien`)
+    if (!who) return m.reply(`Tag or mention someone`)
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) return m.reply(`Agrega el número de días que el usuario sera *Premium*`)
-    if (isNaN(txt)) return m.reply(`Solo números!\n\*Ejemplo*\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
+    if (!txt) return m.reply(`Add the number of days the user will be *Premium*`)
+    if (isNaN(txt)) return m.reply(`Only numbers!\n\*Example*\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
     global.prems.push(`${who.split`@`[0]}`)
     var jumlahHari = 86400000 * txt
     var now = new Date() * 1
     if (now < user.premiumTime) user.premiumTime += jumlahHari
     else user.premiumTime = now + jumlahHari
 user.premium = true
-    m.reply(`*» Nombre* : ${user.name}\n*» Tiempo* :${txt} Días`)
+    m.reply(`*» Name* : ${user.name}\n*» Time* :${txt} Days`)
 }
 handler.help = ['addprem *@user*']
 handler.tags = ['owner']
