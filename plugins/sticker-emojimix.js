9d0a55f8f657bc3382234cffce04eb0ba62d0809
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 18:45:41 +0200

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -12,7 +12,7 @@ resolve(json)
 reject(err)
 })})
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply(`🚩 Ejemplo: *${usedPrefix + command}* 😎+🤑`)
+if (!args[0]) return m.reply(`🐯 Example: *${usedPrefix + command}* 😎+🤑`)
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
 for (let res of anu.results) {
