8ceb68dc65b2edf13a56fafb8c4478de0e1df0d0
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 11:55:08 +0200

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, isOwner }) => {
 let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
 let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
 let caption = `
-╭─「 *Usuarios Baneados* 」
+╭─「 *Banned Users* 」
 ║❥ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
 ║❥ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
 ╰────
 
-╭─「 *Chats Baneados* 」
+╭─「 *Banned Chats* 」
 ║❥ *Total* : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
 ║❥ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
 ╰────
