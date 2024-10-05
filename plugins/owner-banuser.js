a02050546cbcdfad2ab161dc2949b2db92b918ab
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-10-05 12:03:26 +0200

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -5,14 +5,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    if (!who) return m.reply(`🐯 Tag a user.`)
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido baneado con exito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
+    conn.reply(m.chat, `🐯 @${who.split`@`[0]} You have been successfully banned, you will no longer be able to use my commands.`, m, { mentions: [who] })
 }
-handler.help = ['mban *@user*']
+handler.help = ['ban *@user*']
 handler.tags = ['owner']
-handler.command = /^mban$/i
+handler.command = /^ban$/i
 handler.rowner = true
 
 export default handler
