b32fd106d573a4726128e240f37537207ee64893
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-01-01 19:09:21 +0300

    CHUI

--- mselachui/main-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;mselachui;;\nFN:mselachui⁩\nORG:mselachui\nTITLE:\nitem1.TEL;waid=260774358600:260774358600\nitem1.X-ABLabel:mselachui\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:mselachui\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'MSELACHUI03', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
